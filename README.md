61144dfcdec1f428ab474d62126e1f1c6dfae48a
Sebastian Wien <sebastian.wien@posteo.de>
2024-05-07 15:25:42 +0200

    TASK-3: endpoint test added

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# userservice
+# UserService web app
+
+simple web app to fetch data asynchronously from external endpoint https://jsonplaceholder.typicode.com merge the result and provide via JSON.
+
+no persistation, tested with hurl as an file based API endpoint testing tool.
+
+usage:
+
+Debian: ``apt-get install hurl``
+
+Mac: ``brew install hurl``
+
+usage:
+``hurl --test -v user-service.hurl``
+
+output:
+```
+> hurl --test -v user-service.hurl
+user-service.hurl: Running [1/1]
+* ------------------------------------------------------------------------------
+* Executing entry 1
+*
+* Cookie store:
+*
+* Request:
+* GET http://localhost:8080/api/v1/users/1
+*
+* Request can be run with the following curl command:
+* curl 'http://localhost:8080/api/v1/users/1'
+*
+> GET /api/v1/users/1 HTTP/1.1
+> Host: localhost:8080
+> Accept: */*
+> User-Agent: hurl/4.3.0
+>
+* Response: (received 2706 bytes in 76 ms)
+*
+< HTTP/1.1 200
+< Content-Type: application/json
+< Transfer-Encoding: chunked
+< Date: Tue, 07 May 2024 13:21:04 GMT
+<
+*
+* ------------------------------------------------------------------------------
+* Executing entry 2
+*
+* Cookie store:
+*
+* Request:
+* GET http://localhost:8080/api/v1/users/1000
+*
+* Request can be run with the following curl command:
+* curl 'http://localhost:8080/api/v1/users/1000'
+*
+> GET /api/v1/users/1000 HTTP/1.1
+> Host: localhost:8080
+> Accept: */*
+> User-Agent: hurl/4.3.0
+>
+* Response: (received 0 bytes in 35 ms)
+*
+< HTTP/1.1 404
+< Content-Length: 0
+< Date: Tue, 07 May 2024 13:21:04 GMT
+<
+*
+user-service.hurl: Success (2 request(s) in 112 ms)
+--------------------------------------------------------------------------------
+Executed files:  1
+Succeeded files: 1 (100.0%)
+Failed files:    0 (0.0%)
+Duration:        112 ms
+
+```
--- src/main/java/de/sebastian/wien/service/UserServiceClient.java
+++ src/main/java/de/sebastian/wien/service/UserServiceClient.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.time.Duration;
 import java.util.Arrays;
@@ -27,23 +28,27 @@ public class UserServiceClient {
     }
 
     public Optional<User> fetchUserById(Long id) {
-        User fetchedUser = webClient.get()
-            .uri(uriBuilder -> uriBuilder.path("/users/{id}").build(id))
-            .retrieve()
-            .bodyToMono(User.class)
-            .block(Duration.ofSeconds(5));
-
-        if (fetchedUser != null) {
-            UserPost[] postsArray = webClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/posts").queryParam("userId", id).build())
+        try {
+            User fetchedUser = webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/users/{id}").build(id))
                 .retrieve()
-                .bodyToMono(UserPost[].class)
+                .bodyToMono(User.class)
                 .block(Duration.ofSeconds(5));
 
-            if (postsArray != null) {
-                fetchedUser.setPosts(Arrays.asList(postsArray));
+            if (fetchedUser != null) {
+                UserPost[] postsArray = webClient.get()
+                    .uri(uriBuilder -> uriBuilder.path("/posts").queryParam("userId", id).build())
+                    .retrieve()
+                    .bodyToMono(UserPost[].class)
+                    .block(Duration.ofSeconds(5));
+
+                if (postsArray != null) {
+                    fetchedUser.setPosts(Arrays.asList(postsArray));
+                }
             }
+            return Optional.ofNullable(fetchedUser);
+        } catch (WebClientResponseException e) {
+            return Optional.empty();
         }
-        return Optional.ofNullable(fetchedUser);
     }
 }
